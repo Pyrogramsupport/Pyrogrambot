ea21c2aa4af35e47ba2d09f6fcb289e5d90a41ce
Pyrogramsupport <113048903+Pyrogramsupport@users.noreply.github.com>
2024-05-17 15:45:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,7 @@ Siva = Client(
     api_id="19042248",
     api_hash="a1d443cb79941a89c493f22abf4f84cb",
     bot_token="7065912334:AAH-po3qWn85LJXgre0qni4waQgQ3sjsv1g",
-    
+    plugins=dict(root="Bot")
+
+print("Bot is started and perfectly working ")
+Siva.run()
