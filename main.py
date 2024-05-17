5077ee74f5f679fb048eeae81e95fc4e4b0e4b53
Pyrogramsupport <113048903+Pyrogramsupport@users.noreply.github.com>
2024-05-17 07:44:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,18 @@ from pyrogram import Client, filters
 
 
 
-API_ID=""
-API_HASH=""
-TOKEN=""
+API_ID = ""
+API_HASH = ""
+TOKEN = ""
+
+Siva = Client(
+    name="telegrambot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    token=TOKEN
+)
+
+
+
+
+
