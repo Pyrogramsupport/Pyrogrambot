dde785698d53edc7b4aafe2c2a9410ff63ffeaca
Pyrogramsupport <113048903+Pyrogramsupport@users.noreply.github.com>
2024-05-17 07:57:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ from pyrogram import Client, filters
 
 API_ID = "19042248"
 API_HASH = "a1d443cb79941a89c493f22abf4f84cb"
-TOKEN = "7065912334:AAGPjkf_nHW-I7DOrCVZResEFQJNcfBdJ1k"
+BOT_TOKEN = "7065912334:AAGPjkf_nHW-I7DOrCVZResEFQJNcfBdJ1k"
 
 Siva = Client(
     name="telegrambot",
     api_id=API_ID,
     api_hash=API_HASH,
-    token=TOKEN
+    bot_token=BOT_TOKEN
 )
 
 
