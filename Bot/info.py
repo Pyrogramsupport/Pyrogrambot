a024694f64ee1e72fe4a8a8152c31f52539dc79b
Pyrogramsupport <113048903+Pyrogramsupport@users.noreply.github.com>
2024-05-18 09:14:58 +0530

    Update info.py

--- Bot/info.py
+++ Bot/info.py
@@ -7,15 +7,14 @@ from pyrogram.types import Message
 
 @Client.on_message(filters.private & filters.command("info"))
 async def info_cmd(client, message):
-    info_text=f""" ᴜsᴇʀ ɪɴғᴏ 
-________________________________________________
-|| ғɪʀsᴛ ɴᴀᴍᴇ = {message.from_user.first_name}.||
-|| ʟᴀsᴛ ɴᴀᴍᴇ = {message. from_user.last_name}. ||
-|| ғᴜʟʟ ɴᴀᴍᴇ = {message. from_user.fullname}.  ||
-|| ᴜsᴇʀ ɪᴅ = {message. from_user.id}.          ||
-|| ᴜsᴇʀ ɴᴀᴍᴇ = {message.from_user.username}    ||
-|| ᴜsᴇʀ ʟɪɴᴋ = {message.from_user.mention}.     ||                                      
-________________________________________________
+    info_text=f""" <u>ᴜsᴇʀ ɪɴғᴏ </u>
+
+<b>ғɪʀsᴛ ɴᴀᴍᴇ =</b> {message.from_user.first_name}.
+<b>ʟᴀsᴛ ɴᴀᴍᴇ =</b> {message.from_user.last_name}.   
+<b>ғᴜʟʟ ɴᴀᴍᴇ =</b> {message.from_user.fullname}. 
+<b>ᴜsᴇʀ ɪᴅ =</b> {message.from_user.id}.          
+<b>ᴜsᴇʀ ɴᴀᴍᴇ =</b> {message.from_user.username} 
+<b>ᴜsᴇʀ ʟɪɴᴋ =</b> {message.from_user.mention}.                                       
 """
     await message.reply_text(info_text)
     print("User Info Successfully sent to the user")
