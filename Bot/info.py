93d9ec8f82d54048adaec41fa48ef19c8a36bb65
Pyrogramsupport <113048903+Pyrogramsupport@users.noreply.github.com>
2024-05-18 08:56:06 +0530

    Update info.py

--- Bot/info.py
+++ Bot/info.py
@@ -2,16 +2,18 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.private & filters.command("info"))
 async def info_cmd(client, message):
-  info_text = f"""
-ғɪʀsᴛ ɴᴀᴍᴇ = {message.from_user.first_name}.
-ʟᴀsᴛ ɴᴀᴍᴇ = {message. from_user.last_name}.
-ғᴜʟʟ ɴᴀᴍᴇ = {message. from_user.fullname}.
-ᴜsᴇʀ ʟɪɴᴋ = {message. from_user.mention}.
+  info_text =f""" ᴜsᴇʀ ɪɴғᴏ 
+________________________________________________
+|| ғɪʀsᴛ ɴᴀᴍᴇ = {message.from_user.first_name}.||
+|| ʟᴀsᴛ ɴᴀᴍᴇ = {message. from_user.last_name}. ||
+|| ғᴜʟʟ ɴᴀᴍᴇ = {message. from_user.fullname}.  ||
+|| ᴜsᴇʀ ɪᴅ = {message. from_user.id}.          ||
+|| ᴜsᴇʀ ɴᴀᴍᴇ = {message.from_user.username}    ||
+|| ᴜsᴇʀ ʟɪɴᴋ = {message.from_user.mention}.     ||                                      
+________________________________________________
 """
     await message.reply_text(info_text)
     print("User Info Successfully sent to the user")
-
-
 
 
 
