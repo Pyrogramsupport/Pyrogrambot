815b2c8ed0e76d2e2f69761a47d2a11c57c80d05
Pyrogramsupport <113048903+Pyrogramsupport@users.noreply.github.com>
2024-05-23 20:11:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ print("Bot is started and perfectly working ")
 async def main():
     async with Siva:
         await Siva.send_photo(
-            chat_id=-1002123259805,
+            chat_id=-1002242947266,
             photo="https://telegra.ph/file/cefe116aeebdc9462971a.jpg",
             caption="𝐵𝑜𝑡 𝑖𝑠 𝑠𝑡𝑎𝑟𝑡𝑒𝑑 𝑡𝑜 𝑤𝑜𝑟𝑘𝑖𝑛𝑔"
         )
