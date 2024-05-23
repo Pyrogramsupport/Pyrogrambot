cfc02ead945c8bfe38eca5e091fdab232e16155b
Pyrogramsupport <113048903+Pyrogramsupport@users.noreply.github.com>
2024-05-23 15:38:23 +0530

    Update fun.py

--- Bot/fun.py
+++ Bot/fun.py
@@ -1,5 +1,4 @@
 from pyrogram import Client, filters
-from main.py import Siva
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import random 
 
@@ -7,7 +6,7 @@ funList = ["add soon"]
 
 
 
-@Siva.on_message(filters.command("aq"))
+@Client.on_message(filters.command("aq"))
 async def aq_cmd(client, message):
     await message.reply_text(
         text=random.choice(funList)
