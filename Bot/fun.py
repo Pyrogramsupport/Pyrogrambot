66d9821306401e7470d2e6ba1485704d81feb8a2
Pyrogramsupport <113048903+Pyrogramsupport@users.noreply.github.com>
2024-05-23 15:42:11 +0530

    Update fun.py

--- Bot/fun.py
+++ Bot/fun.py
@@ -2,7 +2,14 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import random 
 
-funList = ["add soon"]
+funList = ["Who decided that? - Lelouch Lamperouge, Code Geass",
+           "I'll be the hero! - Izuku Midoriya, My Hero Academia",
+           "See you later, Space Cowboy. - Spike Spiegel, Cowboy Bebop",
+           "I'll do it because I want to, not because I have to. - Yagami Light, Death Note",
+           "The dreams of yesterday are the hopes of today and the reality of tomorrow." - Miyamoto Musashi, Vagabond"
+           
+          
+]
 
 
 
