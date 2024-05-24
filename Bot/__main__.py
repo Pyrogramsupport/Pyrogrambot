09885b214f9f4722681db840039ded6f2df80685
Pyrogramsupport <113048903+Pyrogramsupport@users.noreply.github.com>
2024-05-24 12:14:51 +0530

    Update __main__.py

--- Bot/__main__.py
+++ Bot/__main__.py
@@ -1,4 +1,5 @@
 from Bot import app
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 if __name__ == "__main__":
     print("bot is functioning")
@@ -7,5 +8,9 @@ if __name__ == "__main__":
         app.send_photo(
             chat_id=-1002054576768,
             photo="https://telegra.ph/file/cefe116aeebdc9462971a.jpg",
-            caption="𝐵𝑜𝑡 𝑖𝑠 𝑠𝑡𝑎𝑟𝑡𝑒𝑑 𝑡𝑜 𝑤𝑜𝑟𝑘𝑖𝑛𝑔"
+            caption="𝐵𝑜𝑡 𝑖𝑠 𝑠𝑡𝑎𝑟𝑡𝑒𝑑 𝑡𝑜 𝑤𝑜𝑟𝑘𝑖𝑛𝑔",
+            reply_markup=InlineKeyboardMarkup(
+                [[
+                    InlineKeyboardButton("ADD ME TO YOUR GROUP", url = "https://t.me/sivatheboss_x_robot?startgroup=True")
+                ]]
         )
