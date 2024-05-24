21a2fa12968f58d1fc5e5851e0bae443739a98ce
Pyrogramsupport <113048903+Pyrogramsupport@users.noreply.github.com>
2024-05-24 07:33:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import asyncio
 from pyrogram import Client
 
 async def main():
-Siva = Client(
-    "Pyrogrambot",
-    api_id="19042248",
-    api_hash="a1d443cb79941a89c493f22abf4f84cb",
-    bot_token="7065912334:AAH-po3qWn85LJXgre0qni4waQgQ3sjsv1g",
-    plugins=dict(root="Bot")
-)
+    Siva = Client(
+        "Pyrogrambot",
+        api_id="19042248",
+        api_hash="a1d443cb79941a89c493f22abf4f84cb",
+        bot_token="7065912334:AAH-po3qWn85LJXgre0qni4waQgQ3sjsv1g",
+        plugins=dict(root="Bot")
+    )
 
 print("Bot is started and perfectly working ")
     async with Siva: 
