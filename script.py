c832044ea453517988d36e8640860e2aedeb2fe2
Pyrogramsupport <113048903+Pyrogramsupport@users.noreply.github.com>
2024-05-25 13:29:27 +0530

    Update script.py

--- script.py
+++ script.py
@@ -3,3 +3,8 @@ PM_TEXT = "HEY {} ! I AM SIVA THE  BOSS ROBOT. I AM ANIMATED GROUP MANAGEMENT BO
 SUPPORT_CHAT = "Siva_chat"
 
 HELP_TEXT = "Hello There! Here you can get all of the help commands!"
+
+HELP_BUTTON = [[
+    InlineKeyboardButton("Info", callback_data="info"),
+    InlineKeyboardButton("Day the wishes", callback_data="day")
+]]
